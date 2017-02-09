8d2b71a2b0d582ad9eb0fc2dc276d0468e64685b
Henry <2192705S@student.gla.ac.uk>
2017-02-09 20:50:13 +0000

    category addition begun; forms system started

--- twdproject/urls.py
+++ twdproject/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     #url(r'^about/$', views.about, name='about'),
-    url(r'^category/(?P<category>[\w-]+)/$', views.show_category, name='show_category'),
+	url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w-]+)/$', views.show_category, name='show_category'),
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     # above maps any URLs starting 
