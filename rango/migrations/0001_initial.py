70264d93e9823c7e76a8df73f4fef297bc5e4397
Henry <2192705S@student.gla.ac.uk>
2017-02-09 18:52:56 +0000

    m/t/v begun; category information added, slugs created, admin controls now compensate for slugs

--- rango/migrations/0001_initial.py
+++ rango/migrations/0001_initial.py
@@ -19,6 +19,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=128, unique=True)),
+                ('likes', models.CharField(max_length=128, unique=True)),
             ],
         ),
         migrations.CreateModel(
